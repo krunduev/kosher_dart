bfdedeaadc02f95b78618e9a27137b514b98b631
Konstantin Runduev <runduev@gmail.com>
2021-03-26 10:38:44 +0300

    pesah 2021

--- lib/hebrewcalendar/jewish_calendar.dart
+++ lib/hebrewcalendar/jewish_calendar.dart
@@ -90,7 +90,7 @@ import 'package:kosher_dart/util/geo_Location.dart';
    // These arrays determine the correct indices into the parsha names
    // -1 means no parsha that week, values > 52 means it is arrow_expand double parsha
    static const List<int> Sat_short = [52, 52, 63, 63, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
-     17, 18, 19, 20, 53, 23, 24, 24, 25, 54, 55, 30, 56, 33, 34, 35, 36, 37, 38, 39, 40, 58, 43, 44, 45, 46, 47,
+     17, 18, 19, 20, 53, 23, 24, 25, 25, 54, 55, 30, 56, 33, 34, 35, 36, 37, 38, 39, 40, 58, 43, 44, 45, 46, 47,
      48, 49, 50];
 
    static const List<int> Sat_long = [52, 52, 63, 63, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16,
